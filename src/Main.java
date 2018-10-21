dcce142a1a1e52cc0141b77d1aaefbeab85dcdac
Dmitry Grigoryev <dmitry.grigoryev@gmx.de>
2018-10-21 18:06:37 +0200

    sorted array added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Main {
 
     public static void main(String[] args) {
+
+
+        String [] strings = {"aaa","bbb","a","bb"};
 
         try (InputStream is = new FileInputStream("inputBirthdayList.txt")) {
             int read;
@@ -22,10 +26,18 @@ public class Main {
         Key keyPrice = s -> Integer.parseInt(s.split(",")[s.split(",").length - 1]);
 
 
-        outputFromMapToFile(InputToSortedMapByBirthday("inputbirthdaylist.txt",keyBirthday));
+        outputFromMapToFile(InputToSortedMapByBirthday
+                        ("inputBirthdayList.txt", keyBirthday),
+                "outputBirthdayList.txt");
 
-        outputFromMapToFile(InputToSortedMapByBirthday("gadgets.txt",keyPrice));
-}
+        outputFromMapToFile(InputToSortedMapByBirthday
+                        ("inputGadgetPriceList.txt", keyPrice),
+                "outputGadgetPriceList.txt");
+
+        System.out.println(sumFromFile("numbers.txt"));
+
+        System.out.println(sortedArray(strings));
+    }
 
 
     public static Map<Integer, String> InputToSortedMapByBirthday(String fileName, Key key) {
@@ -42,8 +54,8 @@ public class Main {
     }
 
 
-    public static void outputFromMapToFile(Map<Integer, String> map) {
-        try (BufferedWriter printer = new BufferedWriter(new FileWriter("outputgadgets.txt"))) {
+    public static void outputFromMapToFile(Map<Integer, String> map, String fileName) {
+        try (BufferedWriter printer = new BufferedWriter(new FileWriter(fileName))) {
             for (String s : map.values()) {
                 printer.write(s);
                 printer.newLine();
@@ -52,4 +64,28 @@ public class Main {
             ex.printStackTrace();
         }
     }
+
+    public static Integer sumFromFile(String fileName) {
+        Integer sum = 0;
+        try (BufferedReader is = new BufferedReader(new FileReader(fileName))) {
+            String read;
+            while ((read = is.readLine()) != null) {
+                sum += Integer.parseInt(read);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return sum;
+    }
+    public static List<String> sortedArray (String [] strings){
+        List<String> list = Arrays.asList(strings);
+
+        return  list
+                .stream()
+                .sorted(new StringByLengthComparator())
+                .collect(Collectors.toList());
+
+    }
+
+
 }
